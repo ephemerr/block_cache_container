4193f0f4b1ca6578866860dbc8f80208b1f22340
Alexander Zheleznov <a.zheleznov@gmail.com>
2020-04-14 19:10:29 +0300

    init

---
+++ block_cache_container.cpp
@@ -0,0 +1,8 @@
+#include "block_cache_container.h"
+#include <cstddef>
+#include <QDebug>
+#include <QGlobal>
+
+
+template<typename T>
+B
---
+++ block_cache_container.h
@@ -0,0 +1,10 @@
+#ifndef _BLOCK_CACHE_CONTAINER_H
+#define _BLOCK_CACHE_CONTAINER_H
+
+#include <QList>
+#include <QVector>
+#include <QGlobal>
+#include <cstddef>
+#include <cstring>
+
+#endif /*_BLOCK_CACHE_CONTAINER_H*/
